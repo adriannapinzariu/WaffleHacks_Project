51688bb81caf303cf7b67e264e730184e7d7b329
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-06-24 15:40:14 -0500

    redo with chakraui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import './App.css';
+import { Box, Heading, Text, Link, Button, VStack } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to ASL Learning</h1>
-        <p>Start learning sign language in a fun and easy way!</p>
-        <a
-          className="App-link"
-          href="/get-started" // Update this to your get started route
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Get Started
-        </a>
-      </header>
-    </div>
+    <Box
+      minHeight="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      bgGradient="linear(to-br, lightblue, #E6E6FA)"
+    >
+      <VStack spacing={8} textAlign="center">
+        <Heading fontSize="6xl" color="#4b5178">Welcome to ASL Learning</Heading>
+        <Text fontSize="2xl" color="#4b5178">Start learning sign language in a fun and easy way!</Text>
+        <Link href="/get-started" isExternal>
+          <Button colorScheme="purple" size="lg" color="white">
+            Get Started
+          </Button>
+        </Link>
+      </VStack>
+    </Box>
   );
 }
 
 export default App;
+
 
