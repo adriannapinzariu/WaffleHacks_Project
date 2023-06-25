9ce880e894e05e5d433b8313206d282aa2533779
LightColorz <jojoalemu@gmail.com>
2023-06-25 04:49:08 -0500

    lesson page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StartPage from './StartPage';
 import LoginPage from './LoginPage';
+import LessonPage from './LessonPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<StartPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/lesson" element={<LessonPage />} />
       </Routes>
     </Router>
   );
